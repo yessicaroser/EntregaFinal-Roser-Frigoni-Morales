567cd4cdd8b910b3b190d6636cc20f395768f32b
Yessica Roser <yessica.roser@gmail.com>
2022-10-30 23:46:57 -0300

    Creación modelo Comentario

--- WebDjango/core/models.py
+++ WebDjango/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
+class Comentario(models.Model):
+    nombre_usuario = models.CharField(max_length=50)
+    fecha_comentario = models.DateField(default = None)
+    ingreso_comentario = models.TextField(max_length=1000)
