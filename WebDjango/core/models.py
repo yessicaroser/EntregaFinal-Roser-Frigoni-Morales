2c837c03e04c4077baf755be06d254da71b96356
Constanza Morales A <61435709+conimorales@users.noreply.github.com>
2022-10-31 14:18:16 -0300

    Update models.py
    revision modelos

--- WebDjango/core/models.py
+++ WebDjango/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Publicacion(models.Model):
+
+    titulo = models.CharField(max_length=200)
+    Descripcion = models.CharField(max_length=200)
+    Contenido = models.TextField()
+    fecha_publicacion = models.DateField()
+    hashtag = models.DateField()
+    autor = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.titulo
