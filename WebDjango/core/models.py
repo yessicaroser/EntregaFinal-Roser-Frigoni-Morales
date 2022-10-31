075178d90d22e6bd02d1340b1f750b95f15d37a5
Yessica Roser <yessica.roser@gmail.com>
2022-10-31 00:27:21 -0300

    Se agrega modelo Categoria

--- WebDjango/core/models.py
+++ WebDjango/core/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from datetime import datetime
 
 
-# Create your models here.
-
 class Comentario(models.Model):
+    # Campos
     nombre_usuario = models.CharField(max_length=50)
     fecha_comentario = models.DateField(default = None)
-    ingreso_comentario = models.TextField(max_length=1000)
+    ingreso_comentario = models.TextField(max_length=2000, help_text="Puedes ingresar un comentario hasta 2000 caracteres")
+
+    # Métodos
+    def __str__(self):
+        return f'{self.nombre_usuario} - {self.fecha_comentario} - {self.ingreso_comentario}'
+
+class Categoria(models.Model):
+    # Campos
+    nombre_categoria = models.CharField(max_length=50)
+    descripcion_categoria = models.CharField(max_length=100)
+
+    # Métodos
+    def __str__(self):
+        return f'{self.nombre_categoria} - {self.descripcion_categoria}'
