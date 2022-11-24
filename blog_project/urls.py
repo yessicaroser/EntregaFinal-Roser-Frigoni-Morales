f96fbecfe96958c8613babeaa779d00f08b0e063
Yessica <yessica.roser@gmail.com>
2022-11-24 08:30:48 -0300

    Se agrega opción de carga de imágenes para encabezado de post individuales

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,3 +24,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/', include('AppRegistro.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
