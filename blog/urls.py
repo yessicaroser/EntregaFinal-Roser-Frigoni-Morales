6bc097c6ce0b66a1800ebbf085545fe76d6a179e
Yessica <yessica.roser@gmail.com>
2022-11-26 10:27:19 -0300

    Se organizan páginas en navbar y se corrigen visualizaciones de contenido

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 #from .views import SignUpView
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.HomeView.as_view(), name='home'),
     #path('login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
     #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     #path('signup/', views.SignUp.as_view(), name='signup'),
+    path('post_list/', views.PostListView.as_view(), name='post_list'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('post/<pk>', views.PostDetailView.as_view(), name='post_detail'),
