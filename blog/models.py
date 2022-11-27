ffca832b652cf50a8cfa72133bcc1496e17d43b6
Yessica <yessica.roser@gmail.com>
2022-11-27 16:50:23 -0300

    Carga de texto real en templates

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,7 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.models import User
-from ckeditor_uploader.fields import RichTextUploadingField
-from django.template.defaultfilters import slugify 
+from ckeditor.fields import RichTextField 
 
 
 # Create your models here.
@@ -17,12 +16,12 @@ class Users(auth.models.User, auth.models.PermissionsMixin):
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     imagen_portada = models.ImageField(default='default.jpg', upload_to='images/', null=True, blank=True)
-    contenido = models.RichTextField()
+    contenido = RichTextField(blank=True, null=True)
     fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_publicacion = models.DateTimeField(blank=True, null=True)
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     avatar = models.ImageField(default='default.jpg', upload_to='avatars/', null=True)
-    slug = models.SlugField(max_length=200,unique=True)
+    
     
     def publish(self):
         self.fecha_publicacion = timezone.now()
